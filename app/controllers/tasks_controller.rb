323cf7d70e30323eb93890f92e57fb19e2d2f574
Titti M <tmalmivi@cs.helsinki.fi>
2014-04-02 15:45:51 +0300

    navigointia helpommaks

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -15,6 +15,7 @@ class TasksController < ApplicationController
   # GET /tasks/new
   def new
     @task = Task.new
+    @categories = Category.all
   end
 
   # GET /tasks/1/edit
--- app/models/category.rb
+++ app/models/category.rb
@@ -3,4 +3,8 @@ class Category < ActiveRecord::Base
   has_many :tasks, through: :tasks_categories
 
   validates :name, presence: true
+
+  def to_s
+    "#{name}"
+  end
 end
